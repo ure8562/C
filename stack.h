06c1bf52130b727e2ff2f8500f3ca58eb9453f8b
ure8562 <55536830+ure8562@users.noreply.github.com>
2019-09-28 23:48:09 +0900

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,18 @@
+#ifndef STACK_H
+#define	 STACK_H
+
+#define STACKSIZE 100
+typedef struct stack{
+	//int array[STACKSIZE];
+	int *pArr;
+	int size;
+	int tos;
+} Stack;
+
+void push(Stack *ps, int data);
+int pop(Stack *ps);
+void initStack(Stack *ps, int size);
+void cleanupStack(Stack *ps);
+//...
+
+#endif
